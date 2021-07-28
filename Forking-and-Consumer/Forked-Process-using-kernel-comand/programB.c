60b8654dda7de65644ad6b7d913577c292c9668b
Kylepurcell10 <73486286+Kylepurcell10@users.noreply.github.com>
2021-07-28 19:47:50 -0400

    Operating Systems 2020

---
+++ Forking-and-Consumer/Forked-Process-using-kernel-comand/programB.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+ void main(int argc , char* argv[] , char* envp[]){
+ printf( "Running some program B\n");
+exit(0);
+} 
